7e281e71466adedff8de6caa2f9e2fc637829758
radianika <radianika@gmail.com>
2018-11-15 21:13:34 +0200

    minor fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,36 @@
-//глобальные
-var temp = document.querySelector('#cat-template');
+//global elements
+
+
+var request = new XMLHttpRequest();
+
+// templates
+var card_temp = document.querySelector('.js-catCard-template');
+var bagItem_temp = document.querySelector('.js-shopingBag-template');
+
+//DOM-elements
 var plate = document.querySelector('.cats_plate');
 var moreCats = document.querySelector('.js-addCat');
 var cartCount = document.querySelector('.js-cats-in-bag');
-var catsCountWord = document.querySelector('.js');
-var request = new XMLHttpRequest();
-var page = 1;
-var catsCount = 1;
 var bag = document.querySelector('#shoping-bag');
 var bagWindow = document.querySelector('.js-shopping-cart');
 
-
-
 var closeBag = document.querySelector('.close-btn');
-	closeBag.addEventListener('click', function(){
-		document.querySelector('.bag').classList.add('closed')});
-//получение данных от сервера
+
+//array for cats cards bg
+var bgColors = ['#FED6BC', '#FFFADD', '#DEF7FE', '#E7ECFF', '#C3FBD8', '#FDEED9', '#B5F2EA', '#C6D8FF', '#d3e8ff'];
+// array for shopping bag items
+var catsInBag = [];
+
+//counts
+var page = 1;
+var catsCount = 1;
+
+
+var catsCountWord = document.querySelector('.js');
+
+
+//main function for receiving data from the server and ...
+
 
 function addFromServer() {
 	request.open('GET', 'https://ma-cats-api.herokuapp.com/api/cats?page=' + (page++) + '&per_page=50', true);
@@ -25,8 +40,8 @@ function addFromServer() {
 
 			var resp = JSON.parse(request.responseText);
 			var catsData = resp.cats;
-			// и создание  котиков
-			createCat(catsData);
+			// ... creating cats cards
+			createCatCard(catsData);
 			contentHeight = document.querySelector('body').offsetHeight;
 		} else {
 
@@ -38,73 +53,84 @@ function addFromServer() {
 }
 
 
-// функция для получения рандомного числа
-function randomInteger(min, max) {
-	var rand = min + Math.random() * (max - min)
-	rand = Math.round(rand);
-	return rand;
-}
-
-//массив с цветами для котиков
-var bgColors = ['#FED6BC', '#FFFADD', '#DEF7FE', '#E7ECFF', '#C3FBD8', '#FDEED9', '#B5F2EA', '#C6D8FF', '#d3e8ff'];
-// определение окончания для корзины
-function declOfNum(number, titles) {
-	cases = [2, 0, 1, 1, 1, 2];
-	return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
-}
- 
-
-
-var catsToBuy = [];
-var catForCart={};
-//создание карточки для котиков
-function createCat(data) {
+//creating cards for cats and...
+function createCatCard(data) {
 	for (let i = 0; i < data.length; i++) {
-		var card = document.importNode(temp.content, true);
-		var price = card.querySelector('.js-price');
-		var cat = card.querySelector('.cats-card');
-		var img = card.querySelector('.js-img');
-		var cart = card.querySelector('.js-cart');
-		cart.addEventListener('click', function () {
+		let card = document.importNode(card_temp.content, true);
+		let card_price = card.querySelector('.js-price');
+		card_price.innerHTML = data[i].price;
+
+		let cat = card.querySelector('.cats-card');
+		let img = card.querySelector('.js-img');
+		let card_cartBtn = card.querySelector('.js-cart');
+
+		// ... listener for bag btn in header
+		card_cartBtn.addEventListener('click', function () {
 			cartCount.innerHTML = catsCount;
-			catsCountWord.innerHTML = declOfNum(catsCount, ['котик', 'котика', 'котиков']);
+			catsCountWord.innerHTML = endings(catsCount, ['котик', 'котика', 'котиков']);
 			catsCount++;
 		});
-//		cart.addEventListener('click', addTocart);
 
-		price.innerHTML = data[i].price;
 		img.setAttribute('src', data[i].img_url);
-		var randomBackground = randomInteger(0, 8);
-		cat.style.background = bgColors[randomBackground];
-			cart.addEventListener('click', function(){
+		let randomBackgroundIndex = randomInteger(0, 8);
+		cat.style.background = bgColors[randomBackgroundIndex];
+		// ... card info for shopping bag
+		card_cartBtn.addEventListener('click', function () {
+			let catForCart = {};
 			catForCart.price = data[i].price;
 			catForCart.src = data[i].img_url;
-				let clone = Object.assign({}, catForCart);
-			catsToBuy.push(clone);
-				console.log(catsToBuy);
-				addTocart(catsToBuy);
+			let clone = Object.assign({}, catForCart);
+			catsInBag.push(clone);
+			addToCart(catsInBag);
 		});
 		plate.appendChild(card);
 	}
 }
+//creating bag item
 
-function addTocart(data){
-	for(let i=0; i<data.length; i++){
-	var item=document.importNode(document.querySelector('#shoping-bag-temp').content, true);
-	var del = item.querySelector('.item-delete');
-	var bagImg = item.querySelector('.item-img');
-	bagImg.setAttribute('src', data[i].src);
-	var plus = item.querySelector('.plus-btn');
-	var input = item.querySelector('.input');
-	var minus = item.querySelector('.minus-btn');
-	var total = item.querySelector('.total-price');
+function addToCart(data) {
+	for (let i = 0; i < data.length; i++) {
+		var bagItem = document.importNode(bagItem_temp.content, true);
+		let delBtn = bagItem.querySelector('.item-delete');
+		// delete item onclick
+		delBtn.addEventListener('click', function(){
+			this.parentNode.remove();
+		})
+		let bagImg = bagItem.querySelector('.item-img');
+		bagImg.setAttribute('src', data[i].src);
+	
+		let plus = bagItem.querySelector('.plus-btn');
+		let input = bagItem.querySelector('.input');
+		let minus = bagItem.querySelector('.minus-btn');
+		let total = bagItem.querySelector('.total-price');
 		total.innerHTML = data[i].price;
 	}
-		bagWindow.appendChild(item);
+	bagWindow.appendChild(bagItem);
 }
-//закрыть корзину
 
-	
+
+
+
+
+
+//secondary functions for ...
+
+// ... 1. Random Number for cats cards background.
+function randomInteger(min, max) {
+	var randomNumber = Math.random() * (max - min);
+	return Math.round(randomNumber);
+}
+
+// ... 2. ending for nouns in bag btn
+function endings(number, titles) {
+	cases = [2, 0, 1, 1, 1, 2];
+	return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+}
+
+
+
+
+
 
 //добавить при загрузке
 window.onload = addFromServer();
@@ -121,12 +147,18 @@ window.onscroll = scroll;
 function scroll() {
 	//	 var scrolled = window.pageYOffset || document.documentElement.scrollTop;
 	//  document.getElementById('showScroll').innerHTML = scrolled + 'px';
-	if (window.pageYOffset > (contentHeight-500)) {
+	if (window.pageYOffset > (contentHeight - 500)) {
 		addFromServer();
 		page++;
 	}
 };
+//listeners
+
+bag.addEventListener('click', function () {
+	document.querySelector('.bag').classList.toggle('closed')
+});
 
 
-bag.addEventListener('click', function(){
-document.querySelector('.bag').classList.toggle('closed')});
+closeBag.addEventListener('click', function () {
+	document.querySelector('.bag').classList.add('closed')
+});
